9f1c3f7863bb69bb4f13da6ac3cfc2b7f3c47f32
Anson Cheung <ansoncwork@gmail.com>
2019-10-12 09:04:32 +0100

    first

---
+++ README.md
@@ -0,0 +1,28 @@
+# What is this?
+
+An example of Vue component packaged in npm.
+
+## npm
+
+`npm i @ansonhkg/vue-example-component`
+
+## yarn
+
+`yarn add @ansonhkg/vue-example-component`
+
+Then...
+
+# Usage
+
+```html
+
+<template>
+    <ExampleComponent/>
+</template>
+
+<script>
+    import ExampleComponent from '@ansonhkg/vue-example-component'
+    Vue.use(ExampleComponent)
+</script>
+
+```
---
+++ src/ExampleComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="example-component--container">
+    <span>This is an example component.</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "example-component",
+  mounted() {
+    console.warn("Example Component is mounted.");
+  }
+};
+</script>
+
+<style>
+.example-component--container {
+  background-color: red;
+  width: 200px;
+  height: 200px;
+  display: flex;
+  justify-content: center;
+  padding: 10px;
+  border: 10px solid black;
+  box-sizing: border-box;
+  text-align: center;
+  position: fixed;
+}
+.example-component--container > span {
+  margin: auto;
+}
+</style>
